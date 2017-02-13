6dd0034102a74e29cdb9fcfa630ae29ac9ff1da5
Vadym <vadosevich@gmail.com>
2017-02-13 09:23:29 +0200

    Make tabs, needs repair margin-right

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -7,4 +7,61 @@ $(document).ready(function(){
 		navText: ['','']
 
 	});
+
+	/*--- (Portfokio) Pop-up image ---*/
+
+	$('.tab_content_elem').magnificPopup({
+		type:'image'
+	});
+
+	/*--- (Portfokio) Tabs ---*/
+
+	var tab_elem =  $('.tab_content_elem');
+	var tab_content = $('.tab_content');
+
+	function MarginRight(px){
+		tab_content.each(function(){
+			$(this).children().css({'margin-right':' ' + px + 'px'});
+		})
+	};
+
+	function MarginRightZero(){
+		var elementsQuantity = Math.ceil( tab_content.width() / tab_elem.width() - 1) ;
+		tab_content.children()+$(':nth-child('+elementsQuantity+'n)').css('margin-right','0');
+	};
+
+	function addTabContent() {
+	
+		$('.tab_links ul li a').click(function(e){
+			
+			var tab_id = $(this).attr('data-tab');
+			var tabClass = $('.'+ tab_id);
+
+			e.preventDefault();
+
+			tab_content.children().each(function(){
+				if(tab_id != 'all') {
+					$(this).hide();
+				} else {
+					$(this).show();
+				}
+			})
+
+			tabClass.css('display','block');	
+
+			MarginRight(20);
+		  	MarginRightZero();	
+
+		});
+	};
+
+	$(window).resize(function() {
+		MarginRight(20);
+	  	MarginRightZero();
+	});
+
+	addTabContent(); 
+	
+
 });
+
--- gulpfile.js
+++ gulpfile.js
@@ -29,10 +29,9 @@ gulp.task('css-libs', ['sass'], function(){
 gulp.task('scripts', function(){
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
-		'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js',
 		'app/libs/bootstrap/dist/js/bootstrap.min.js',
-		'app/libs/owl.carousel/dist/owl.carousel.js',
-		'app/libs/masonry/dist/masonry.pkgd.min.js'
+		'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js',
+		'app/libs/owl.carousel/dist/owl.carousel.js'
 	])
 	.pipe(concat('libs.min.js'))
 	.pipe(uglify())
